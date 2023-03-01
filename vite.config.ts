9ab11ae570167a26b56bf3c6f48aa47ab3d57e21
tanahashiyasuhiro <yasuhiro.tanahashi@vitalize.co.jp>
2023-03-02 03:44:21 +0900

    Viteがaliasを解決できるようにviteの設定を追記

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port:8080
+  },
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
